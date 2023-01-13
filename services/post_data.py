dded48170818ac81cd4b51cc06b922695caf2887
Richard Bignell <bignellrp@gmail.com>
2023-01-13 18:51:13 +0000

    Added try and except to post data

--- services/post_data.py
+++ services/post_data.py
@@ -13,13 +13,16 @@ def wipe_tally():
     players = player()
     all_players = players.all_players()
     for name,totals in all_players:
-        player_table.update_item(
-            Key={'Name': name},
-            UpdateExpression="set Playing=:p",
-            ExpressionAttributeValues={
-                ':p': 'o'},
-            ReturnValues="UPDATED_NEW"
-        )
+        try:
+            player_table.update_item(
+                Key={'Name': name},
+                UpdateExpression="set Playing=:p",
+                ExpressionAttributeValues={
+                    ':p': 'o'},
+                ReturnValues="UPDATED_NEW"
+            )
+        except Exception as msg:
+            print(f"Oops, could not update: {msg}")
     print("Wiping tally!")
     return
 
@@ -27,58 +30,64 @@ def update_tally(values):
     '''Function to update the player 
     tally using the values from the index page'''
     for name in values:
-        player_table.update_item(
-            Key={'Name': name},
-            UpdateExpression="set Playing=:p",
-            ExpressionAttributeValues={
-                ':p': 'x'},
-            ReturnValues="UPDATED_NEW"
-        )
+        try:
+            player_table.update_item(
+                Key={'Name': name},
+                UpdateExpression="set Playing=:p",
+                ExpressionAttributeValues={
+                    ':p': 'x'},
+                ReturnValues="UPDATED_NEW"
+            )
+        except Exception as msg:
+            print(f"Oops, could not update: {msg}")
     print("Updated tally!")
     return
 
 def update_result(values):
     '''Function to update the result row 
     using the values from the results page'''
-    results_table.update_item(
-        Key={'Date': next_wednesday},
-        UpdateExpression="set #1=:1, #2=:2, #3=:3, #4=:4, #5=:5, #6=:6, #7=:7, #8=:8, #9=:9, #10=:10, #11=:11, #12=:12, #13=:13, #14=:14, #15=:15, #16=:16",
-        ExpressionAttributeNames={
-            '#1': 'Team A Result?',
-            '#2': 'Team B Result?',
-            '#3': 'Team A Total',
-            '#4': 'Team B Total',
-            '#5': 'Team A Player 1',
-            '#6': 'Team A Player 2',
-            '#7': 'Team A Player 3',
-            '#8': 'Team A Player 4',
-            '#9': 'Team A Player 5',
-            '#10': 'Team B Player 1',
-            '#11': 'Team B Player 2',
-            '#12': 'Team B Player 3',
-            '#13': 'Team B Player 4',
-            '#14': 'Team B Player 5',
-            '#15': 'Team A Colour',
-            '#16': 'Team B Colour'},
-        ExpressionAttributeValues={
-            ':1': values[1],
-            ':2': values[2],
-            ':3': values[3],
-            ':4': values[4],
-            ':5': values[5],
-            ':6': values[6],
-            ':7': values[7],
-            ':8': values[8],
-            ':9': values[9],
-            ':10': values[10],
-            ':11': values[11],
-            ':12': values[12],
-            ':13': values[13],
-            ':14': values[14],
-            ':15': values[15],
-            ':16': values[16]},
-        ReturnValues="UPDATED_NEW"
-    )
+    try:
+        results_table.update_item(
+            Key={'Date': next_wednesday},
+            UpdateExpression="set #1=:1, #2=:2, #3=:3, #4=:4, #5=:5, #6=:6, #7=:7, #8=:8, #9=:9, #10=:10, #11=:11, #12=:12, #13=:13, #14=:14, #15=:15, #16=:16",
+            ExpressionAttributeNames={
+                '#1': 'Team A Result?',
+                '#2': 'Team B Result?',
+                '#3': 'Team A Total',
+                '#4': 'Team B Total',
+                '#5': 'Team A Player 1',
+                '#6': 'Team A Player 2',
+                '#7': 'Team A Player 3',
+                '#8': 'Team A Player 4',
+                '#9': 'Team A Player 5',
+                '#10': 'Team B Player 1',
+                '#11': 'Team B Player 2',
+                '#12': 'Team B Player 3',
+                '#13': 'Team B Player 4',
+                '#14': 'Team B Player 5',
+                '#15': 'Team A Colour',
+                '#16': 'Team B Colour'},
+            ExpressionAttributeValues={
+                ':1': values[1],
+                ':2': values[2],
+                ':3': values[3],
+                ':4': values[4],
+                ':5': values[5],
+                ':6': values[6],
+                ':7': values[7],
+                ':8': values[8],
+                ':9': values[9],
+                ':10': values[10],
+                ':11': values[11],
+                ':12': values[12],
+                ':13': values[13],
+                ':14': values[14],
+                ':15': values[15],
+                ':16': values[16]},
+            ReturnValues="UPDATED_NEW"
+        )
+    except Exception as msg:
+        print(f"Oops, could not update: {msg}")
     wipe_tally()
     return
 
@@ -88,27 +97,30 @@ def append_result(values):
     Takes in values to be added 
     and returns the command for 
     appending the data'''
-    results_table.put_item(
-        Item={
-        'Date': values[0],
-        'Team A Result?': values[1],
-        'Team B Result?': values[2],
-        'Team A Total': values[3],
-        'Team B Total': values[4],
-        'Team A Player 1': values[5],
-        'Team A Player 2': values[6],
-        'Team A Player 3': values[7],
-        'Team A Player 4': values[8],
-        'Team A Player 5': values[9],
-        'Team B Player 1': values[10],
-        'Team B Player 2': values[11],
-        'Team B Player 3': values[12],
-        'Team B Player 4': values[13],
-        'Team B Player 5': values[14],
-        'Team A Colour': values[15],
-        'Team B Colour': values[16]
-        }
-    )
+    try:
+        results_table.put_item(
+            Item={
+            'Date': values[0],
+            'Team A Result?': values[1],
+            'Team B Result?': values[2],
+            'Team A Total': values[3],
+            'Team B Total': values[4],
+            'Team A Player 1': values[5],
+            'Team A Player 2': values[6],
+            'Team A Player 3': values[7],
+            'Team A Player 4': values[8],
+            'Team A Player 5': values[9],
+            'Team B Player 1': values[10],
+            'Team B Player 2': values[11],
+            'Team B Player 3': values[12],
+            'Team B Player 4': values[13],
+            'Team B Player 5': values[14],
+            'Team A Colour': values[15],
+            'Team B Colour': values[16]
+            }
+        )
+    except Exception as msg:
+        print(f"Oops, could not update: {msg}")
     wipe_tally()
     return
 
@@ -119,16 +131,20 @@ def update_score_result(values):
     returns the gspread command for updating row
     Updates both Score A and Score B 
     from a list of two values.'''
-    results_table.update_item(   
-        Key={'Date': next_wednesday, 'Team A Result?': '-'},
-        UpdateExpression="set #1=:1, #2=:2",
-        ExpressionAttributeNames={
-            '#1': 'Team A Result?',
-            '#2': 'Team B Result?'},
-        AttributeUpdates={
-            ':1': values[0],
-            ':2': values[1]}
-    )
+    try:
+        results_table.update_item(   
+            Key={'Date': next_wednesday},
+            UpdateExpression="set #1=:1, #2=:2",
+            ExpressionAttributeNames={
+                '#1': 'Team A Result?',
+                '#2': 'Team B Result?'},
+            ExpressionAttributeValues={
+                ':1': values[0],
+                ':2': values[1]},
+            ReturnValues="UPDATED_NEW"
+        )
+    except Exception as msg:
+        print(f"Oops, could not update: {msg}")
     update_formulas()
     return
 
@@ -136,14 +152,18 @@ def update_scorea(value):
     '''Function to update the result using 
     the values from the results page
     Takes in value to be added to the table updates item'''
-    results_table.update_item(   
-        Key={'Date': next_wednesday, 'Team A Result?': '-'},
-        UpdateExpression="set #1=:1",
-        ExpressionAttributeNames={
-            '#1': 'Team A Result?'},
-        AttributeUpdates={
-            ':1': value}
-    )
+    try:
+        results_table.update_item(   
+            Key={'Date': next_wednesday},
+            UpdateExpression="set #1=:1",
+            ExpressionAttributeNames={
+                '#1': 'Team A Result?'},
+            ExpressionAttributeValues={
+                ':1': value},
+            ReturnValues="UPDATED_NEW"
+        )
+    except Exception as msg:
+        print(f"Oops, could not update: {msg}")
     update_formulas()
     return
 
@@ -151,14 +171,18 @@ def update_scoreb(value):
     '''Function to update the result using 
     the values from the results page
     Takes in value to be added to the table updates item'''
-    results_table.update_item(   
-        Key={'Date': next_wednesday, 'Team B Result?': '-'},
-        UpdateExpression="set #1=:1",
-        ExpressionAttributeNames={
-            '#1': 'Team B Result?'},
-        AttributeUpdates={
-            ':1': value}
-    )
+    try:
+        results_table.update_item(   
+            Key={'Date': next_wednesday},
+            UpdateExpression="set #1=:1",
+            ExpressionAttributeNames={
+                '#1': 'Team B Result?'},
+            AttributeUpdates={
+                ':1': value},
+            ReturnValues="UPDATED_NEW"
+        )
+    except Exception as msg:
+        print(f"Oops, could not update: {msg}")
     update_formulas()
     return
 
@@ -166,40 +190,51 @@ def update_coloura(value):
     '''Function to update the result using 
     the values from the results page
     Takes in value to be added to the table updates item'''
-    results_table.update_item(   
-        Key={'Date': next_wednesday, 'Team A Result?': '-'},
-        UpdateExpression="set #1=:1",
-        ExpressionAttributeNames={
-            '#1': 'Team A Colour'},
-        AttributeUpdates={
-            ':1': value}
-    )
+    try:
+        results_table.update_item(   
+            Key={'Date': next_wednesday},
+            UpdateExpression="set #1=:1",
+            ExpressionAttributeNames={
+                '#1': 'Team A Colour'},
+            AttributeUpdates={
+                ':1': value},
+            ReturnValues="UPDATED_NEW"
+        )
+    except Exception as msg:
+        print(f"Oops, could not update: {msg}")
     return
 
 def update_colourb(value):
     '''Function to update the result using 
     the values from the results page
     Takes in value to be added to the table updates item'''
-    results_table.update_item(   
-        Key={'Date': next_wednesday, 'Team A Result?': '-'},
-        UpdateExpression="set #1=:1",
-        ExpressionAttributeNames={
-            '#1': 'Team B Colour'},
-        AttributeUpdates={
-            ':1': value}
-    )
+    try:
+        results_table.update_item(   
+            Key={'Date': next_wednesday},
+            UpdateExpression="set #1=:1",
+            ExpressionAttributeNames={
+                '#1': 'Team B Colour'},
+            AttributeUpdates={
+                ':1': value},
+            ReturnValues="UPDATED_NEW"
+        )
+    except Exception as msg:
+        print(f"Oops, could not update: {msg}")
     return
 
 def update_playing_status(player):
     '''Takes in a player 
     and adds x into the playing column'''
-    player_table.update_item(
-        Key={'Name': player},
-        UpdateExpression="set Playing=:p",
-        ExpressionAttributeValues={
-            ':p': 'x'},
-        ReturnValues="UPDATED_NEW"
-    )
+    try:
+        player_table.update_item(
+            Key={'Name': player},
+            UpdateExpression="set Playing=:p",
+            ExpressionAttributeValues={
+                ':p': 'x'},
+            ReturnValues="UPDATED_NEW"
+        )
+    except Exception as msg:
+        print(f"Oops, could not update: {msg}")
     print("Updated playing status for:",player)
     return
 
@@ -220,32 +255,37 @@ def add_new_player(player):
     '''Appends New Row with a new 
     player and generic score'''
     new_player = [player,int(77)] 
-
-    player_table.update_item(
-        Key={'Name': player},
-        UpdateExpression="set Name=:n, Total=:t, Wins=:w, Draws=:d, Losses=:l, Score=:s, Played=:p, #pc=:pc, #wp=:wp",
-        ExpressionAttributeNames={
-            '#pc': 'Percent Calc',
-            '#wp': 'Win Percentage'},
-        ExpressionAttributeValues={
-            ':n': new_player[0],
-            ':t': new_player[1],
-            ':w': '0',
-            ':d': '0',
-            ':l': '0',
-            ':s': '0',
-            ':p': '0',
-            ':pc': '0',
-            ':wp': '0'},
-        ReturnValues="UPDATED_NEW"
-    )
+    try:
+        player_table.update_item(
+            Key={'Name': player},
+            UpdateExpression="set Name=:n, Total=:t, Wins=:w, Draws=:d, Losses=:l, Score=:s, Played=:p, #pc=:pc, #wp=:wp",
+            ExpressionAttributeNames={
+                '#pc': 'Percent Calc',
+                '#wp': 'Win Percentage'},
+            ExpressionAttributeValues={
+                ':n': new_player[0],
+                ':t': new_player[1],
+                ':w': '0',
+                ':d': '0',
+                ':l': '0',
+                ':s': '0',
+                ':p': '0',
+                ':pc': '0',
+                ':wp': '0'},
+            ReturnValues="UPDATED_NEW"
+        )
+    except Exception as msg:
+        print(f"Oops, could not update: {msg}")
     print("Added new player called:",player)
     return
 
 def remove_player(player):
     '''Deletes player from player table'''
-    player_table.delete_item(
-        Key={'Name': player}
-    )
+    try:
+        player_table.delete_item(
+            Key={'Name': player}
+        )
+    except Exception as msg:
+        print(f"Oops, could not update: {msg}")
     print("Deleted",player)
     return
